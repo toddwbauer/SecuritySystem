c0112f0560f3e1f56e5b2ce38a1b67c7ef8cc38d
Todd Bauer <twbauer8220@eagle.fgcu.edu>
2020-11-30 18:24:45 -0500

    Improved comments for clarity.

--- scripts/user.py
+++
@@ -1,8 +0,0 @@
-class User:
-    
-    def __init__(self, user_name, password, access_level):
-        self.user_name = name
-        self.password = password
-        self.access_level = access_level
-        
-    
